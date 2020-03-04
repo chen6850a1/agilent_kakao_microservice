859c28f4c72192c9d6de6e4851eb1b0db41f9bc9
chenhong <hong.chen5@non.agilent.com>
2020-03-04 16:13:07 +0800

    定义常量

---
+++ src/Cts/Common/ConstParam.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hongch02
+ * Date: 2020/1/23
+ * Time: 20:59
+ */
+
+namespace Cts\Common;
+
+
+class ConstParam
+{
+  const USER_TYPE_KAKAO="user_type_kakao";
+  const USER_TYPE_ADMIN="user_type_admin";
+}
--- src/Cts/Common/Lib/UserInterface.php
+++ src/Cts/Common/Lib/UserInterface.php
@@ -17,10 +17,6 @@ namespace Cts\Common\Lib;
  */
 interface UserInterface
 {
-    const TYPE_KAKAO="tyoe_kakao_user";
-    const TYPE_ADMIN="type_admin_user";
-
-
     /**
      * 前端用户登录
      * @param string $type
