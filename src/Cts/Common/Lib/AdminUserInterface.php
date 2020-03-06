0d6069b92fc898b3fd5472016abfee32e47baff5
nanye <nanye2362@163.com>
2020-03-06 10:45:38 +0800

    注释补充接口请求数据格式

--- src/Cts/Common/Lib/AdminUserInterface.php
+++ src/Cts/Common/Lib/AdminUserInterface.php
@@ -14,41 +14,92 @@ interface AdminUserInterface {
     /**
      * 
      * @param array $param
+     * @example:{
+     *      account:string,
+     *      password:string
+     * }
+     * 
      * @return array
+     * @example {
+     *      status:true|false,
+     *      data:string,
+     *      error:string
+     * }
      */
     public function login(array $param): array;
 
     /**
      * 
      * @param array $params
+     * @example:{
+     *      password:string
+     * }
+     * 
      * @return array
+     * @example {
+     *      status:true|false,
+     *      data:string,
+     *      error:string
+     * }
      */
     public function changePassword(array $params): array;
 
     /**
      * 
      * @param array $params
+     * @example:{
+     *      page:string|int,
+     *      pageSize:string|int,
+     *      keyword:string
+     * }
+     * 
      * @return array
+     * @example {
+     *      status:true|false,
+     *      data:string,
+     *      error:string
+     * }
      */
     public function getList(array $params): array;
 
     /**
      * 
      * @param int $uid
+     * 
      * @return array
+     * @example {
+     *      status:true|false,
+     *      data:string,
+     *      error:string
+     * }
      */
     public function get(int $uid): array;
 
     /**
      * 
      * @return array
+     * @example {
+     *      status:true|false,
+     *      data:string,
+     *      error:string
+     * }
      */
     public function getAuthority(): array;
 
     /**
      * 
      * @param array $params
+     * @example {
+     *      account:string,
+     *      access:string eg.[1,4,8,11]
+     * }
+     * 
      * @return array
+     * @example {
+     *      status:true|false,
+     *      data:string,
+     *      error:string
+     * }
      */
     public function create(array $params): array;
 
@@ -56,14 +107,29 @@ interface AdminUserInterface {
      * 
      * @param int $uid
      * @param array $params
+     * @example {
+     *      access:string eg.[1,4,8,11]
+     * }
+     * 
      * @return array
+     * @example {
+     *      status:true|false,
+     *      data:string,
+     *      error:string
+     * }
      */
     public function update(int $uid, array $params): array;
 
     /**
      * 
      * @param int $uid
+     * 
      * @return array
+     * @example {
+     *      status:true|false,
+     *      data:string,
+     *      error:string
+     * }
      */
     public function delete(int $uid): array;
 }
--- src/Cts/Common/Lib/SliderInterface.php
+++ src/Cts/Common/Lib/SliderInterface.php
@@ -14,20 +14,42 @@ interface SliderInterface {
     /**
      * 
      * @return array
+     * @example {
+     *      status:true|false,
+     *      data:string,
+     *      error:string
+     * }
      */
     public function getList(): array;
 
     /**
      * 
      * @param int $id
+     * 
      * @return array
+     * @example {
+     *      status:true|false,
+     *      data:string,
+     *      error:string
+     * }
      */
     public function get(int $id): array;
 
     /**
      * 
      * @param array $params
+     * @example {
+     *      title:string,
+     *      src:file,
+     *      link:string
+     * }
+     * 
      * @return array
+     * @example {
+     *      status:true|false,
+     *      data:string,
+     *      error:string
+     * }
      */
     public function create(array $params): array;
 
@@ -35,7 +57,18 @@ interface SliderInterface {
      * 
      * @param int $id
      * @param array $params
+     * @example {
+     *      title:string,
+     *      src:file,
+     *      link:string
+     * }
+     * 
      * @return array
+     * @example {
+     *      status:true|false,
+     *      data:string,
+     *      error:string
+     * }
      */
     public function update(int $id, array $params): array;
 
@@ -43,20 +76,41 @@ interface SliderInterface {
      * 
      * @param int $id
      * @param array $params
+     * @example {
+     *      order:string|int,
+     *      operator:string eg. +|-
+     * }
+     * 
      * @return array
+     * @example {
+     *      status:true|false,
+     *      data:string,
+     *      error:string
+     * }
      */
     public function updateOrder(int $id, array $params): array;
 
     /**
      * 
      * @param int $id
+     * 
      * @return array
+     * @example {
+     *      status:true|false,
+     *      data:string,
+     *      error:string
+     * }
      */
     public function delete(int $id): array;
 
     /**
      * 
      * @return array
+     * @example {
+     *      status:true|false,
+     *      data:string,
+     *      error:string
+     * }
      */
     public function publish(): array;
 }
