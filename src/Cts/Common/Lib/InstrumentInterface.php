915031e2bdf9f83e727969ee54fee3ccb0f8567c
hong.chen <hong.chen5@non.agilent.com>
2020-04-05 13:27:47 +0800

    更新仪器服务

--- src/Cts/Common/Lib/InstrumentInterface.php
+++ src/Cts/Common/Lib/InstrumentInterface.php
@@ -25,4 +25,61 @@ interface InstrumentInterface {
      * ]
      */
     public function ocr(array $data): array;
+
+
+    /**
+     * 获取suvery
+     * @param array $data
+     * @example:[
+     *      sn:XXXXX
+     * ]
+     *
+     * @return array
+     * @example {
+     *      status:true,
+     *      data:XXXX
+     * ]
+     */
+    public function addSn(array $data): array;
+
+    /**
+     * 获取suvery
+     * @param array $data
+     * @example:[
+     *      image_data:base64 字符串
+     * ]
+     *
+     * @return array
+     * @example {
+     *      status:true,
+     *      data:XXXX
+     * ]
+     */
+    public function delSn(array $data): array;
+
+    /**
+     * 获取suvery
+     *
+     * @return array
+     * @example {
+     *      status:true,
+     *      data:XXXX
+     * ]
+     */
+    public function getSnList(): array;
+
+    /**
+     * 获取suvery
+     * @param array $data
+     * @example:[
+     *      image_data:base64 字符串
+     * ]
+     *
+     * @return array
+     * @example {
+     *      status:true,
+     *      data:XXXX
+     * ]
+     */
+    public function getSnDetail(array $data): array;
 }
