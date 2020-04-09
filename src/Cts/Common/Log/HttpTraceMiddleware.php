e0e0ce106188b0c6e71aec6e5341184488765083
hong.chen <hong.chen5@non.agilent.com>
2020-04-09 08:28:33 +0800

    更新日志链路追踪

---
+++ src/Cts/Common/Log/HttpTraceMiddleware.php
@@ -0,0 +1,65 @@
+<?php declare(strict_types=1);
+
+
+namespace Cts\Common\Log;
+
+
+use Psr\Http\Message\ServerRequestInterface;
+use Swoft\Bean\Annotation\Mapping\Bean;
+use Swoft\Log\Helper\Log;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Swoft\Http\Server\Contract\MiddlewareInterface;
+
+/**
+ * Class HttpTraceMiddleware
+ *
+ * @since 2.0
+ *
+ * @Bean()
+ */
+class HttpTraceMiddleware implements MiddlewareInterface
+{
+    /**
+     * @param ServerRequestInterface        $request
+     * @param RequestHandlerInterface $requestHandler
+     *
+     * @return ResponseInterface
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $requestHandler): ResponseInterface
+    {
+        $this->startRequest($request);
+        $response = $requestHandler->handle($request);
+        $this->endRequest();
+
+        return $response;
+    }
+
+    public function startRequest(ServerRequestInterface $request) {
+        context()->set('startTime', microtime(true));
+        context()->set('interface', $request->getUri()->getPath());
+        context()->set('method', $request->getMethod());
+
+        context()->set('appInfo', [
+            'env'     => config('env'),
+            'name'    => config('name'),
+            'version' => config('version'),
+        ]);
+    }
+
+    public function endRequest() {
+        //计算整个请求耗时
+        $cost         = sprintf('%.2f', (microtime(true)-context()->get('startTime')) * 1000);
+        context()->set('cost', $cost . 'ms');
+
+        //记录请求参数
+        $params = [
+            'query' => context()->getRequest()->input(),
+            'header' => context()->getRequest()->getHeaders(),
+            'sql'  => context()->get('sql', '无')
+        ];
+        context()->set('params', $params);
+
+        Log::info('Http接口请求结束');
+    }
+}
---
+++ src/Cts/Common/Log/RpcTraceMiddleware.php
@@ -0,0 +1,69 @@
+<?php declare(strict_types=1);
+
+
+namespace Cts\Common\Log;
+
+
+use Swoft\Bean\Annotation\Mapping\Bean;
+use Swoft\Log\Helper\Log;
+use Swoft\Rpc\Server\Contract\MiddlewareInterface;
+use Swoft\Rpc\Server\Contract\RequestHandlerInterface;
+use Swoft\Rpc\Server\Contract\RequestInterface;
+use Swoft\Rpc\Server\Contract\ResponseInterface;
+
+/**
+ * Class RpcTraceMiddleware
+ *
+ * @since 2.0
+ *
+ * @Bean()
+ */
+class RpcTraceMiddleware implements MiddlewareInterface
+{
+    /**
+     * @param RequestInterface        $request
+     * @param RequestHandlerInterface $requestHandler
+     *
+     * @return ResponseInterface
+     */
+    public function process(RequestInterface $request, RequestHandlerInterface $requestHandler): ResponseInterface
+    {
+        $this->startRpc($request);
+        $response = $requestHandler->handle($request);
+        $this->endRpc();
+
+        return $response;
+    }
+
+    public function startRpc(RequestInterface $request)
+    {
+        context()->set('startTime', microtime(true));
+        context()->set('version', $request->getVersion());
+        context()->set('interface', $request->getInterface());
+        context()->set('method', $request->getMethod());
+        $params = [
+            'query' => $request->getParams(),
+            'sql' => context()->get('sql', '')
+        ];
+        context()->set('params', $params);
+        context()->set('appInfo', [
+            'env' => config('env'),
+            'name' => config('name'),
+            'version' => config('version'),
+        ]);
+    }
+
+    public function endRpc()
+    {
+        //计算耗时时间
+        $cost = sprintf('%.2f', (microtime(true) - context()->get('startTime')) * 1000);
+        context()->set('cost', $cost . 'ms');
+
+        //获取执行sql
+        $params = context()->get('params');
+        $params['sql'] = context()->get('sql', '');
+        context()->set('params', $params);
+
+        Log::info(sprintf("【%s】服务，RPC 请求结束", config('name', 'swoft')));
+    }
+}
