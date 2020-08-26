3a235f55be08f03f8d116c3e50d3f3ce35f80773
hong.chen <283255625@qq.com>
2020-08-26 09:59:00 +0800

    更新用户信息接口

--- src/Cts/Common/Lib/InstrumentInterface.php
+++ src/Cts/Common/Lib/InstrumentInterface.php
@@ -406,5 +406,15 @@ interface InstrumentInterface {
      */
     public function instumentExport(array $data):array;
 
+    /**
+     * 根据用户uid获取最后一个序列号详情
+     *
+     * @return array
+     * @example {
+     *      status:true,
+     *      data:XXXX
+     * ]
+     */
+    public function getLastSnDetailByUid(int $uid):array;
 
 }
