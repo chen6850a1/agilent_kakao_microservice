f168140e688e6487e76d7cf44da6f7612cda6bb2
hong.chen <283255625@qq.com>
2020-12-25 09:32:26 +0800

    增加日志的长度限制

--- src/Cts/Common/Aws/AwsKinesisFirehose.php
+++ src/Cts/Common/Aws/AwsKinesisFirehose.php
@@ -48,7 +48,7 @@ class AwsKinesisFirehose
                 echo "\n";
             }
         }else{
-            if(strlen($content)<1000){
+            if(strlen($content)<2000){
                 CLog::info($content);
             }else{
                 CLog::info("日志内容过多，应性能暂不保存");
