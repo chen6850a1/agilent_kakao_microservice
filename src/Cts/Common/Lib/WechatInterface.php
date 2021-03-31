dc7f90aa056ef6821fa662a5de13a237ceb062f5
sky <595931496@qq.com>
2021-03-31 22:41:24 +0800

    获取离线消息的参数id

--- src/Cts/Common/Lib/WechatInterface.php
+++ src/Cts/Common/Lib/WechatInterface.php
@@ -228,7 +228,8 @@ interface WechatInterface
 
     /**
      * 获取微信scheme码
-     * @param $jumpWxa  ["path" => "/pages/index/index","query" => "a=1&b=2"]
+     * @param $path   /pages/index/index
+     * @param $query   a=1&b=2
      * @param bool $isExpire 生成的scheme码类型，到期失效：true，永久有效：false。
      * @param int $expireTime 到期失效的scheme码的失效时间，为Unix时间戳。生成的到期失效scheme码在该时间前有效。最长有效期为1年。生成到期失效的scheme时必填。
      * @return array
@@ -241,5 +242,12 @@ interface WechatInterface
      *          ]
      * ]
      */
-    public function urlSchemeGenerate($jumpWxa,$isExpire = false,$expireTime = 0):array;
+    /**
+     * @param $path
+     * @param $query
+     * @param bool $isExpire
+     * @param int $expireTime
+     * @return array
+     */
+    public function urlSchemeGenerate(string $path,string $query,bool $isExpire = false, int $expireTime = 0):array;
 }
