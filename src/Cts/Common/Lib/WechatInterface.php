b949a33d6b77d637e8c49cc70a08a89ccfc4e365
sky <595931496@qq.com>
2021-08-05 16:23:41 +0800

    ''

--- src/Cts/Common/Lib/WechatInterface.php
+++ src/Cts/Common/Lib/WechatInterface.php
@@ -356,23 +356,23 @@ interface WechatInterface {
     public function LoginInEnterpriseWechat($userId): array;
 
     /**
-     * 查看用户是否有未读的SOBOT聊天消息
+     * 获取用户sobot未读消息的数量
      * @param $data
      * @return array
      */
-    public function checkSobotUnreadMessage($data):array;
+    public function getSobotUnreadMessageCount($data):array;
 
     /**
      * 用户加入SOBOT聊天
      * @param $data
-     * @return bool
+     * @return array
      */
     public function joinSobotChat($data):array;
 
     /**
      * 用户离开SOBOT聊天
      * @param $data
-     * @return bool
+     * @return array
      */
     public function leaveSobotChat($data):array;
 }
