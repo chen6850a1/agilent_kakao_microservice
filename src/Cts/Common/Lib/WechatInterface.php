d75cb0f0ceee801874cf76303a5858c02ecb6c26
nanye <nanye2362@163.com>
2021-09-08 17:17:57 +0800

    修复合并代码问题

--- src/Cts/Common/Lib/WechatInterface.php
+++ src/Cts/Common/Lib/WechatInterface.php
@@ -720,6 +720,25 @@ interface WechatInterface {
      * @example {
      *      status:true,
      *      data:[
+     *          "cdnUrl": "http://mmbiz.qpic.cn/mmbiz_jpg/FVribAGdErI2jhO1hbzVDH1E5LW7VQ9D1SIvTLFmyYwkIUucJqE72icgAj6NYw92kbJRNV0zwnGZWaO0Y8T0vqlg/0", // 分享二维码
+     *          "pagePath": "plugin-private://wx2b03c6e691cd7370/pages/live-player-plugin?room_id=6209", // 分享路径
+     *          "posterUrl": "http://mmbiz.qpic.cn/mmbiz_jpg/dlFudV139LtQn96YNHSGtVfcUkUwQaYeR5OiaJ6bPr7OZxy3yhIK3icjxop0QeRBiaDPiaxuzENtCj15zFIM8sh7ag/0", // 分享海报
+     *      ]
+     *  }
+     * */
+    public function getSharedCode(array $params): array;
+
+    /**
+     * @param array $params
+     * @example [
+     *      "params" => encodeURIComponent(JSON.stringify(custom_params)),
+     *      "roomId" => 15
+     *  ]
+     *
+     * @return array
+     * @example {
+     *      status:true,
+     *      data:[
      *           "errcode"=>  0,  //微信返回的状态码
      *           "errmsg"=> "ok",  //微信返回的错误信息
      *          "openlink"=> Scheme, //ticket码
