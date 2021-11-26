f58ddfecb1148715c4fd5c9ec54adae39680150c
hong.chen <283255625@qq.com>
2021-11-26 17:05:23 +0800

    更新 关闭本地文件 日志

--- src/Cts/Common/Aspect/FileLogAspect.php
+++ src/Cts/Common/Aspect/FileLogAspect.php
@@ -7,26 +7,18 @@ namespace Cts\Common\Aspect;
 use App\Annotation\Parser\LogRegister;
 use Swoft\Aop\Annotation\Mapping\Around;
 use Swoft\Aop\Annotation\Mapping\Aspect;
-use Swoft\Aop\Annotation\Mapping\Before;
 use Swoft\Aop\Annotation\Mapping\PointExecution;
 use Swoft\Aop\Point\ProceedingJoinPoint;
-use Swoft\Aop\Proxy;
-use Swoft\Bean\BeanFactory;
-use Swoft\Context\Context;
-use Swoft\Db\DB;
-use Swoft\Log\Helper\CLog;
-use Swoft\Log\Logger;
-use Swoft\Stdlib\Helper\ArrayHelper;
+
 /**
  * @Aspect(order=1)
  * @PointExecution(
- *     include={"Logger::appendNoticeLog"},
+ *     include={"FileHandler::handleBatch"},
  *     exclude={}
  * )
  */
-class NoticeLoggerAspect
+class FileLogAspect
 {
-
     /**
      * @Around()
      *
