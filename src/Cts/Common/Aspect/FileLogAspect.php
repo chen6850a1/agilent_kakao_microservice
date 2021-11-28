2e0145ab52d5406b7ff429c28150f9f1fe2c0a6e
hong.chen <283255625@qq.com>
2021-11-28 13:03:49 +0800

    更新 过滤日志 功能

--- src/Cts/Common/Aspect/FileLogAspect.php
+++ src/Cts/Common/Aspect/FileLogAspect.php
@@ -7,9 +7,17 @@ namespace Cts\Common\Aspect;
 use App\Annotation\Parser\LogRegister;
 use Swoft\Aop\Annotation\Mapping\Around;
 use Swoft\Aop\Annotation\Mapping\Aspect;
+use Swoft\Aop\Annotation\Mapping\Before;
 use Swoft\Aop\Annotation\Mapping\PointExecution;
 use Swoft\Aop\Point\ProceedingJoinPoint;
-
+use Swoft\Aop\Proxy;
+use Swoft\Bean\BeanFactory;
+use Swoft\Context\Context;
+use Swoft\Db\DB;
+use Swoft\Log\Handler\FileHandler;
+use Swoft\Log\Helper\CLog;
+use Swoft\Log\Logger;
+use Swoft\Stdlib\Helper\ArrayHelper;
 /**
  * @Aspect(order=1)
  * @PointExecution(
@@ -17,8 +25,9 @@ use Swoft\Aop\Point\ProceedingJoinPoint;
  *     exclude={}
  * )
  */
-class FileLogAspect
+class NoticeLoggerAspect
 {
+
     /**
      * @Around()
      *
