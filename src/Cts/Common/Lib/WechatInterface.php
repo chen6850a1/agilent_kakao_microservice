c527f823f7a2a9b6cba36ee17c37c3cca6220a2a
sky <595931496@qq.com>
2022-03-31 10:39:01 +0800

    通过微信获取手机号

--- src/Cts/Common/Lib/WechatInterface.php
+++ src/Cts/Common/Lib/WechatInterface.php
@@ -1093,4 +1093,11 @@ interface WechatInterface {
      * @return array
      */
     public function checkCompanyDiff(array $params): array;
+
+    /**
+     * 通过微信获取手机号
+     * @param string $code
+     * @return array
+     */
+    public function getPhoneByWechat(string $code) :array;
 }
